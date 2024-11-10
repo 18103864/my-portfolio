cfe5dfb50942e3717d97052f01fcd6ecabe1f608
cahrlloyloy <18103864@usc.edu.ph>
2024-11-10 21:46:59 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Projects',
+    href: '#projects',
+  },
+  {
+    id: 4,
+    name: 'Work',
+    href: '#work',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Carl-endly',
+    desc: 'Carl-endly is a user-friendly web application inspired by Calendly, designed to streamline event scheduling and appointments. ',
+    subdesc:
+      'Users can create personalized events, set available time slots, and share links for others to easily schedule meetings without the hassle of back-and-forth messaging. With intuitive scheduling options, Carl-Endly simplifies the process of managing appointments, offering a seamless experience for both organizers and attendees.',
+    href: 'https://carl-endly.vercel.app/',
+    github: 'https://github.com/18103864/carl-endly',
+    texture: '/textures/project/carl-endly.mp4',
+    logo: '/assets/carl-endly.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Nextjs',
+        path: '/assets/nextjs.png',
+      },
+      {
+        id: 2,
+        name: 'React',
+        path: 'assets/react.svg',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Tailwind',
+        path: '/assets/tailwindcss.png',
+      },
+      {
+        id: 5,
+        name: 'Shadcnui',
+        path: '/assets/shadcn.png',
+      },
+    ],
+  },
+  {
+    title: 'CarlDocs',
+    desc: 'Carl Docs is a collaborative document-editing web application modeled after Google Docs.',
+    subdesc:
+      ' It enables users to create, edit, and share documents in real-time with others. With features for live editing, commenting, and version control, Carl Docs offers a seamless platform for individuals and teams to work together on documents, fostering efficient collaboration and productivity in one convenient space.',
+    href: 'https://carl-docs.vercel.app',
+    github: 'https://github.com/18103864/carl-docs',
+    texture: '/textures/project/carl-docs.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Nextjs',
+        path: '/assets/nextjs.png',
+      },
+      {
+        id: 2,
+        name: 'React',
+        path: 'assets/react.svg',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Tailwind',
+        path: '/assets/tailwindcss.png',
+      },
+      {
+        id: 5,
+        name: 'Shadcnui',
+        path: '/assets/shadcn.png',
+      },
+    ],
+  },
+  {
+    title: 'Carl-AI',
+    desc: 'Carl-AI is an innovative image-generation platform powered by artificial intelligence, allowing users to create unique visuals from text prompts.',
+    subdesc:
+      'By entering descriptive prompts, users can generate customized images that suit their creative needs. Once generated, images can be seamlessly posted and shared within the platform, fostering a community of creativity and inspiration. Carl-AI empowers users to turn their ideas into visual art effortlessly.',
+    href: 'https://carlai.fun',
+    github: 'https://github.com/18103864/carlAI',
+    texture: '/textures/project/carl-ai.mp4',
+    logo: '/assets/carl-ai.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'Node',
+        path: '/assets/node.png',
+      },
+      {
+        id: 4,
+        name: 'OpenAi',
+        path: '/assets/openai.png',
+      },
+    ],
+  },
+  {
+    title: 'Carl Connect',
+    desc: 'Carl Connect is a dynamic social media platform designed for real-time sharing and engaging with content.',
+    subdesc:
+      'Users can post updates, share thoughts, media, and connect with friends, communities, and influencers. Whether you are looking to share personal moments, follow interests, or engage in global discussions, Carl Connect makes it easy to stay connected in the digital world.',
+    href: 'https://y-lhj4.onrender.com',
+    github: 'https://github.com/18103864/y',
+    texture: '/textures/project/carl-connect.mp4',
+    logo: '/assets/carlconnect.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'Node',
+        path: '/assets/node.png',
+      },
+      {
+        id: 4,
+        name: 'Shadcnui',
+        path: '/assets/shadcn.png',
+      },
+    ],
+  },
+  {
+    title: 'Todd Avatar-Maker',
+    desc: 'Todd Avatar-Maker is a fun and interactive web app that allows users to customize outfits for "Todd," a character inspired by the $TODD Memecoin launched on PumpFun.',
+    subdesc:
+      'With a variety of clothing options and accessories, users can create unique profile pictures (PFPs) of Todd. Whether for social media or just for fun, Todd-PFP Maker offers a creative way to engage with the $TODD community.',
+    href: 'https://toddpfpmaker.netlify.app/',
+    github: 'https://github.com/18103864/toddmememaker',
+    texture: '/textures/project/todd-pfpmaker.mp4',
+    logo: '/assets/todd.jpg',
+    logoStyle: {
+      backgroundColor: '#dedeaa',
+      border: '0.2px solid #a4a150',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Html',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/css.png',
+      },
+      {
+        id: 3,
+        name: 'Javascript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Freelance Web Development',
+    pos: 'Full Stack/Frontend Developer',
+    duration: '2024',
+    title: "As a freelance front-end developer, I focused on creating dynamic and responsive memecoin landing pages using React. In parallel, I expanded my skillset by learning full-stack development, working with the MERN stack and Next.js to build more robust and scalable applications. This combination of front-end expertise and full-stack learning allowed me to deliver high-quality, user-focused solutions while growing my technical abilities.",
+    icon: '/assets/webdev.png',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Freelance Graphic Design',
+    pos: 'Graphic Artist',
+    duration: '2024',
+    title: "As a freelance graphic designer, I created engaging and creative memes for Solana memecoins, leveraging pop culture references and humor to boost community engagement and brand visibility. My work involved designing eye-catching visuals that resonated with crypto enthusiasts, helping to drive social media interaction and support the growth of the Solana memecoin ecosystem.",
+    icon: '/assets/graphic-artist.png',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Pixel8',
+    pos: 'Junior Front End Developer',
+    duration: '2023 - 2024',
+    title: "As a Junior Front-End Developer at Pixel8, I assisted in building responsive web applications, writing clean code with React, Vue, and Quasar. I collaborated with the team to implement features, optimize performance, and ensure cross-browser compatibility, all while focusing on delivering a seamless user experience.",
+    icon: '/assets/pixel8.png',
+    animation: 'salute',
+  },
+];
+
+
+
+
